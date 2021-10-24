8e6232722283fa196a3ffffd0dddf93410967475
komarovma <komarovma@hotmail.com>
2021-10-24 14:19:49 +0300

    tf7.2

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+.tf
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
