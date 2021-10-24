710d2dadff8e87a2f78aa7f491b39a9e088637a2
komarovma <komarovma@hotmail.com>
2021-10-24 20:37:58 +0300

    tf7.3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,53 @@
 # main.tf
 
-
+locals {
+  web_instance_count_map = {
+    stage = 1
+    prod = 2
+  }
+  instances = {
+  "t3.micro" = "${data.aws_ami.ubuntu.id}"
+  "t3.medium" = "${data.aws_ami.ubuntu.id}"
+}
+}
 provider "aws" {
   profile = "default"
-  region  = var.aws_region
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -26,9 +70,18 @@ data "aws_ami" "ubuntu" {
 data "aws_caller_identity" "current" {}
 resource "aws_instance" "app_server" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-
-  tags = {
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+  count = local.web_instance_count_map[terraform.workspace]
+    tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_instance" "app_server_for" {
+  for_each = local.instances 
+      ami = each.value
+      instance_type = each.key
+      tags = {
+      Name = var.instance_name
+      }
 }
