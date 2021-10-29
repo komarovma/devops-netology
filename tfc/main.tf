81b9f1d8b034417d7179b6becee70abbb5969079
komarovma <komarovma@hotmail.com>
2021-10-29 19:48:56 +0300

    7.4

---
+++ tfc/main.tf
@@ -0,0 +1,36 @@
+# main.tf
+
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-impish-21.10-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+data "aws_caller_identity" "current" {}
+resource "aws_instance" "app_server" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+
---
+++ tfc/outputs.tf
@@ -0,0 +1,43 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+output "instance_ami" {
+  description = "AMI of the EC2 instance"
+  value       = aws_instance.app_server.ami
+}
+
+output "instance_arn" {
+  description = "ARN the EC2 instance"
+  value       = aws_instance.app_server.arn
+}
+output "instance_private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.app_server.private_ip
+}
+
+output "instance_network_id" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.app_server.subnet_id
+}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "region" {
+  value = var.aws_region
+}
